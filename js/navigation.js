8a9e3c31e99a6437e121c9e819d3c2c2f5549a4b
Hal4506 <hal.propp@gmail.com>
2018-08-28 21:56:35 -0500

    More responsive updates

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,3 @@
-var header = document.getElementById("mainNav");
-var btns = header.getElementsByClassName("w3-button");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-
 
 function aboutNavClicked() {
    var x = document.getElementById("aboutNav");
